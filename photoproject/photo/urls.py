e98193d126eed7d1d6118e4fac99719fecda60f5
otani0423 <yuha_otani@totec.co.jp>
2023-06-01 13:28:17 +0900

    photo/urls.py

---
+++ photoproject/photo/urls.py
@@ -0,0 +1,15 @@
+# p359 photo/urls.py
+
+from django.urls import path
+from . import views
+
+# URLパターンを逆引きできるように名前を付ける
+app_name  = 'photo'
+
+# URLパターンを登録する変数
+urlpatterns = [
+    # photoアプリへのアクセスはviewsモジュールのIndexViewを実行
+    path('', views.IndexView.as_view(), name='index'),
+]
+
+#テスト
