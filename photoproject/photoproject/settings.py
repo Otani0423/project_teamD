4c783dbd1dcc5f1a2c17baed025a9bc364d2c6b6
otani0423 <yuha_otani@totec.co.jp>
2023-06-01 14:05:16 +0900

    accounts

--- photoproject/photoproject/settings.py
+++ photoproject/photoproject/settings.py
@@ -15,8 +15,6 @@ from pathlib import Path
 import os
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
